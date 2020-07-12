a6cbaa6e532238c20d9fa4928e0c2adee7df070c
DRMD431LU <drmdfoureilu@gmail.com>
2020-07-12 16:17:19 -0500

    create api view

--- restapi/status/api/urls.py
+++ restapi/status/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include, re_path
-from .views import StatusAPIView
+from .views import StatusAPIView, StatusCreateAPIView
 
 urlpatterns = [
     path('api', StatusAPIView.as_view()),
-    # path('create/', StatusCreateAPIView.as_view()),
+    path('create/', StatusCreateAPIView.as_view()),
     # re_path(r'^(P<id>.*)/$', StatusCreateAPIView.as_view()),
     # re_path(r'^(P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
     # re_path(r'^(P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
